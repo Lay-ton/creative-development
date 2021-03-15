bd89af0dcbffc6ed69f338c1f0b7f1b8487b6833
Lay-ton <laytonseal@gmail.com>
2021-03-15 18:26:52 -0500

    added actions and services

---
+++ src/services/user.service.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/auth/";
+
+const register = (username, email, password) => {
+    return axios.post(API_URL + "signup", {
+        username,
+        email, 
+        password,
+    });
+};
+
+const login = (username, password) => {
+    return axios.post(API_URL + "signin", {
+        username,
+        password,
+    }). then((response) => {
+        if (response.data.accessToke) {
+            localStorage.setItem("user", JSON.stringify(response.data));
+        }
+
+        return response.data;
+    })
+}
+
+const logout = () => {
+    localStorage.removeItem("user");
+};
+
+export default {
+    register,
+    login,
+    logout,
+};
